d67370a0073e675d96d9722abf8e643ecdf8f326
fraserbrown87 <fraserbrown87@gmail.com>
2017-11-06 21:52:14 +0000

    Part B half of  Questions now answered

--- specs/student_spec.rb
+++ specs/student_spec.rb
@@ -2,34 +2,59 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../student.rb')
 
-class TestStudent < MiniTest::Test
-  def test_return_name
-    student = Student.new('Fraser', 17)
-    assert_equal('Fraser', student.name)
-  end
-  def test_return_cohort
-    student = Student.new('Fraser', 17)
-    assert_equal(17, student.cohort)
-  end
-  def test_set_name
-    student = Student.new('Fraser', 17)
-    student.set_name('Calum')
-    assert_equal('Calum', student.name)
-  end
-  def test_set_cohort
-    student = Student.new('Fraser', 17)
-    student.set_cohort(20)
-    assert_equal(20, student.cohort)
+# class TestStudent < MiniTest::Test
+#   def test_return_name
+#     student = Student.new('Fraser', 17)
+#     assert_equal('Fraser', student.name)
+#   end
+#   def test_return_cohort
+#     student = Student.new('Fraser', 17)
+#     assert_equal(17, student.cohort)
+#   end
+#   def test_set_name
+#     student = Student.new('Fraser', 17)
+#     student.set_name('Calum')
+#     assert_equal('Calum', student.name)
+#   end
+#   def test_set_cohort
+#     student = Student.new('Fraser', 17)
+#     student.set_cohort(20)
+#     assert_equal(20, student.cohort)
+#   end
+#
+#   def test_input_comment
+#     student = Student.new('Fraser', 17)
+#     assert_equal("I Can Talk!", student.talk)
+#   end
+#
+#   def test_input_favourite_programme
+#     student = Student.new('Fraser', 17)
+#     result = student.fav_programme("Ruby")
+#     assert_equal("I love Ruby", result)
+#   end
+# end
+
+class TestTeam < MiniTest::Test
+
+  def test_return_team
+    sports_team = Team.new('Hibernian', players = ["John McGinn", "Simon Murray", "Anthony Stokes"], "Neil Lennon" )
+    assert_equal('Hibernian', sports_team.team_name)
   end
 
-  def test_input_comment
-    student = Student.new('Fraser', 17)
-    assert_equal("I Can Talk!", student.talk)
+  def test_return_players
+    sports_team = Team.new('Hibernian', players = ["John McGinn", "Simon Murray", "Anthony Stokes"], "Neil Lennon" )
+    assert_equal(players = ["John McGinn", "Simon Murray", "Anthony Stokes"], sports_team.players)
   end
 
-  def test_input_favourite_programme
-    student = Student.new('Fraser', 17)
-    result = student.fav_programme("Ruby")
-    assert_equal("I love Ruby", result)
+  def test_return_coach
+    sports_team = Team.new('Hibernian', players = ["John McGinn", "Simon Murray", "Anthony Stokes"], "Neil Lennon" )
+    assert_equal("Neil Lennon", sports_team.coach)
   end
+
+  def test_set_coach_name
+    sports_team = Team.new('Hibernian', players = ["John McGinn", "Simon Murray", "Anthony Stokes"], "Neil Lennon" )
+    sports_team.set_coach_name("Fraser Brown")
+    assert_equal("Fraser Brown", sports_team.coach)
+  end
+
 end
--- student.rb
+++ student.rb
@@ -1,31 +1,58 @@
-class Student
+# # Part A
+# class Student
+#
+#   def initialize(input_name, input_cohort)
+#     @name = input_name
+#     @cohort = input_cohort
+#   end
+#
+#   def name
+#     return @name
+#   end
+#
+#   def cohort
+#     return @cohort
+#   end
+#
+#   def set_name(name)
+#     @name = name
+#   end
+#
+#   def set_cohort(cohort)
+#     @cohort = cohort
+#   end
+#
+#   def talk
+#     return 'I Can Talk!'
+#   end
+#
+#   def favourite_programme(programme)
+#     return "I love #{programme}"
+#   end
+# end
 
-  def initialize(input_name, input_cohort)
-    @name = input_name
-    @cohort = input_cohort
+# Part B
+class Team
+
+  def initialize(input_team_name, input_players, input_coach)
+    @team_name = input_team_name
+    @players = input_players
+    @coach = input_coach
   end
 
-  def name
-    return @name
+  def team_name
+    return @team_name
   end
 
-  def cohort
-    return @cohort
+  def players
+    return @players
   end
 
-  def set_name(name)
-    @name = name
+  def coach
+    return @coach
   end
 
-  def set_cohort(cohort)
-    @cohort = cohort
-  end
-
-  def talk
-    return 'I Can Talk!'
-  end
-
-  def fav_programme(programme)
-    return "I love #{programme}"
+  def set_coach_name(coach)
+    @coach = coach
   end
 end
