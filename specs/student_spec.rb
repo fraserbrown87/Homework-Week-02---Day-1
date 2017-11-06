bb411cda609b5906f1d62f54f06914e9c097cff7
fraserbrown87 <fraserbrown87@gmail.com>
2017-11-06 17:23:46 +0000

    Part A questions now answered

--- specs/student_spec.rb
+++ specs/student_spec.rb
@@ -11,4 +11,25 @@ class TestStudent < MiniTest::Test
     student = Student.new('Fraser', 17)
     assert_equal(17, student.cohort)
   end
+  def test_set_name
+    student = Student.new('Fraser', 17)
+    student.set_name('Calum')
+    assert_equal('Calum', student.name)
+  end
+  def test_set_cohort
+    student = Student.new('Fraser', 17)
+    student.set_cohort(20)
+    assert_equal(20, student.cohort)
+  end
+
+  def test_input_comment
+    student = Student.new('Fraser', 17)
+    assert_equal("I Can Talk!", student.talk)
+  end
+
+  def test_input_favourite_programme
+    student = Student.new('Fraser', 17)
+    result = student.fav_programme("Ruby")
+    assert_equal("I love Ruby", result)
+  end
 end
--- student.rb
+++ student.rb
@@ -12,4 +12,20 @@ class Student
   def cohort
     return @cohort
   end
+
+  def set_name(name)
+    @name = name
+  end
+
+  def set_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def talk
+    return 'I Can Talk!'
+  end
+
+  def fav_programme(programme)
+    return "I love #{programme}"
+  end
 end
